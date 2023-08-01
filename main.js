d398bb67b08741c746323585fba0abe711596c02
Diangien <106443686+Diangien@users.noreply.github.com>
2023-08-01 20:02:03 -0300

    dddd

--- main.js
+++ main.js
@@ -1,30 +1,96 @@
 let computador_opt = document.getElementById('computador')
 let amigo_opt = document.getElementById('amigo')
-let opcao = ''
 let sl1=0,sl2=0,sl3=0,sc1=0,sc2=0,sc3=0,sdp=0,sds=0
 const x = 'X'
 const o = 'O'
 let clicou=false,game_over=false
+let opcao='amigo'
+let ponto_x_document = document.getElementById('ponto-jogador-1')
+let ponto_o_document = document.getElementById('ponto-jogador-2')
+let btn_novo= document.getElementById('novo-jogo-button')
+let ponto_x=0,ponto_o=0
 let vez=0
-if(amigo_opt.checked == true){
-    opcao = 'amigo'
-}else{
-    opcao = 'pc'
-}
 let btns = document.getElementsByClassName('btn')
 btns = [...btns]
 
 let btn_clicados =[], matriz=[],l1=[],l2=[],l3=[]
 
+//opcoes de muda de escolha
 
+amigo_click =()=> opcao='amigo'
+pc_click = ()=> opcao = 'pc'
+
+
+renovar = function(){
+    vez=0
+    btns.map((el)=>{
+        el.style.color = '#fff';
+        clicou=false
+        el.textContent ='1'
+    })
+
+    btn_clicados.splice(0,btn_clicados.length)
+}
+
+
+let verifica = ()=>{
+    matriz = [[num(btns[0].textContent),num(btns[1].textContent),num(btns[2].textContent)],
+              [num(btns[3].textContent),num(btns[4].textContent),num(btns[5].textContent)],
+              [num(btns[6].textContent),num(btns[7].textContent),num(btns[8].textContent)]]
+
+    sl1 = sl2 = sl3 = sc1 = sc2 = sc3 = sdp = sds = 0;
+    for(let i=0;i<3;i++){
+        for(let j=0;j<3;j++){
+            if(i==0){
+                sl1 = matriz[0][0]+ matriz[0][1] + matriz[0][2]
+            }
+            if(i==2){
+                sl2 = matriz[i][0]+ matriz[i][1] + matriz[i][2]
+            }else if(i==3){
+                sl3 = matriz[i][0]+ matriz[i][1] + matriz[i][2]
+            }
+
+            if(j==0){
+                sc1 += matriz[i][0]
+            }else if(j==2){
+                sc2 += matriz[i][1]  
+            }else if(j==3){
+                sc3 += matriz[i][2]      
+           }
+
+            if(i==j){
+                sdp += matriz[i][j]
+            }
+
+            if(j == Math.Abs(i - 2))
+            {
+                 sds += quadro[i][Math.Abs(i - 2)];
+            }
+            
+        }
+    }
+
+     if(sl1==3|| sl2==3||sl3==3||sc1==3||sc2==3||sc3==3||sdp==3||sds==3){
+         return "X"
+     }else if(sl1==6|| sl2==6||sl3==6||sc1==6||sc2==6||sc3==6||sdp==6||sds==6){
+         return "O"
+     }else if((sl1 + sl2+sl3)==13){
+        return "-1"
+     }
+     else{
+         return "7"
+     }
+}
 //novo jogo
 
-novo_jogo = function(escolha){
+
+
+novo_jogo = function(escolha='amigo'){
+    renovar()
     btns.map((element)=>{
-   
         element.addEventListener('click',(evt)=>{
         let quadro = evt.target
-           if(opcao=='amigo'){
+           if(escolha=='amigo'){
                 if(btn_clicados.includes(quadro)){
                     return     
                 }
@@ -41,84 +107,55 @@ novo_jogo = function(escolha){
            }else{
     
            }
+
+           if(verifica()== 'X'){
+                alert('X ganhou')
+                ponto_x++
+                ganhar('x')
+           }else if(verifica()=='O'){
+                alert('O ganhou')
+                ponto_o++
+                ganhar('o')
+           }else if(verifica()=='-1'){
+                alert('Empate!')
+    
+           }
         })
     })
 }
 //Gerar Numeros aleatorios
 
 let random = (inicio,fim)=> (Math.random() * fim + 1) + inicio
-
-
-
-converter_vez = (vez) => vez == 0?'X':'O'
+converter_vez = vez => vez == 0?'X':'O'
 num = (texto)=>{
     if(texto == "X"){
-        return 3
+        return 1
     }else if(texto=="O"){
-        return 5
+        return 2
     }else{
         return 7
     }
 }
 
-let verifica = ()=>{
-    matriz = [[num(btns[0]),num(btns[1]),num(btns[2])],[num(btns[3]),num(btns[4]),num(btns[5])],[num(btns[6]),num(btns[7]),num(btns[8])]]
-
-    for(let i=0;i<3;i++){
-        for(let j=0;i<3;j++){
-            if(i==0){
-                sl1 = matriz[i][0]+ matriz[i][1] + matriz[i][2]
-            }else if(i==2){
-                sl2 = matriz[i][0]+ matriz[i][1] + matriz[i][2]
-            }else if(i==3){
-                sl3 = matriz[i][0]+ matriz[i][1] + matriz[i][2]
-            }
-
-            if(j==0){
-                sc1 = matriz[0][j]+ matriz[1][j] + matriz[2][j]
-            }else if(j==2){
-                sc2 = matriz[0][j]+ matriz[1][j] + matriz[2][j]
-            }else if(j==3){
-                sc3 = matriz[0][j]+ matriz[1][j] + matriz[2][j]
-            }
-
-            if(i==j){
-                sdp += matriz[i][j]
-            }
-            
-        }
-    }
-
-     if(sl1==6|| sl2==6||sl3==6||sc1==6||sc2==6||sc3==6||sdp==6){
-         return "X"
-     }else if(sl1==15|| sl2==15||sl3==15||sc1==15||sc2==15||sc3==15||sdp==15){
-         return "O"
-     }else{
-         return "1"
-     }
-
-    
+ganhar = ()=>{
 }
 
-//  let tempo = setInterval(()=>{
+perder = ()=>{
+}
 
-//      if(verifica() == "X"){
-//          alert('X ganhou')
-//          clearInterval(tempo)
-//      }else if(verifica() == "O"){
-//          alert('O ganhou')
-//              clearInterval(tempo)
-//      }else if(verifica() == "1"){
-//              alert('Empate')
-//              clearInterval(tempo)
-//      }
-   
-        
-//      }
-//  ,1*60*1000)
+empate = ()=>{
+}
 
+clic = ()=>{
+    alert(sl2)
+}
 
-novo_jogo(opcao)
+btn_novo.addEventListener('click',()=>{
+    novo_jogo('amigo')
+})
+
+novo_jogo('amigo')
+
 
 
 
